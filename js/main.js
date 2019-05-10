1cf293a3d730030b64ddd227b0c734cdd219893c
Tanisuke Goro <keisi1100525@outlook.jp>
2019-05-10 19:47:21 +0900

    PullRecest用のcommit

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ const $id = id => document.getElementById(id);
 
 $("button").click(function() {
     console.log("クリックされました！");
-
+    console.log("初めてpullRecestを");
     const arg1 = $id("arg1").value;
     const arg2 = $id("arg2").value;
     const arg = arg2 !== 0 ? arg1 / arg2 : "ゼロでは割れません";
